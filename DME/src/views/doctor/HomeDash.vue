293852d01ff218dbd67287ee95d6ad544db47643
eDyrr <e6yr@proton.me>
2024-04-24 07:58:14 +0100

    wrote a bit of the doctor's view

---
+++ DME/src/views/doctor/HomeDash.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home-container">
+
+    </div>
+</template>
+<script>
+import WelcomeDoctor from '@/components/WelcomeDoctor.vue'
+import TotalCard from '@/components/TotalCard.vue'
+import DonutChart from '@/components/DonutCircle.vue'
+import PatientCount from '@/components/PatientCount.vue'
+import NoteSheet from '@/components/NoteSheet.vue'
+
+export default {
+    components: {
+        WelcomeDoctor,
+        TotalCard
+    }
+}
+</script>
+<style>
+.home-container {
+    display: grid;
+}
+</style>
